9e1bb63ea991b5417b1b0671173394a30bc8aba0
Vova <vzakharov@gmail.com>
2022-05-03 00:22:14 +0300

    Squashed commit of the following:
    commit af16249147ca03aaa4e6a5c2b596c812ba49c3fc
    Author: Vova <vzakharov@gmail.com>
    Date:   Tue May 3 00:21:41 2022 +0300
    
        add hr, remove sorts mill

--- components/Editor.vue
+++ components/Editor.vue
@@ -63,7 +63,8 @@
               headingLevel = headingRegex.exec(paragraph)?.[1].length,
               bulletRegex = /^([\*-])(\s+)/,
               numberedRegex = /^(\d+)\.\s+/,
-              ctaRegex = /^(\[)(.+)(\])$/
+              ctaRegex = /^(\[)(.+)(\])$/,
+              hrRegex = /^(---)$/
 
             if ( !this.disableFormatting ) {
 
@@ -85,6 +86,8 @@
                 paragraph = paragraph.replace( ctaRegex, 
                   `<button class="btn btn-primary">${grayOut('$1')}$2${grayOut('$3')}</button>`
                 )
+              } else if ( hrRegex.test(paragraph) ) {
+                attributes = ' class="hr"'
               }
 
               // If the paragraph is empty, add a heigh 1em attribute to the paragraph tag
@@ -371,7 +374,7 @@
 
   #editor-wrapper {
     font-size: 1.1em;
-    font-family: 'Sorts Mill Goudy', 'Georgia', serif;
+    font-family: 'Georgia', serif;
     /* Rounded shadow, no borders */
     border-radius: 5px;
 
@@ -450,6 +453,16 @@
     border-left: 4px solid #ddd;
     margin-left: 1em;
     padding-left: 1em;
+  }
+
+  * >>> .hr {
+    /* Gray and small */
+    color: #ccc;
+    font-size: 0.8em;
+    margin-top: 3em;
+    margin-bottom: 2em;
+    border: 0;
+    border-top: 1px solid rgba(0, 0, 0, 0.1);
   }
   
 
